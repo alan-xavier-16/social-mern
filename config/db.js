a2bced3ac8b3f218d36365906e8c52244ef19cc3
Alan Xavier <alan_xvr@hotmail.com>
2020-01-19 19:46:09 -0400

    Connect to MongoDB 1. Create a config directory for configuration files and variables 2. Create db.js to handle connecting to MongoDB using mongoose 3. Import connectDB() into server.js

---
+++ config/db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { mongoURI } = require("./default");
+
+/** Connecting Mongoose to MongoDB */
+const connectDB = async () => {
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1); // Exit process with a failure
+  }
+};
+
+module.exports = connectDB;
