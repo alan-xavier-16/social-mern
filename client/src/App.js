5dcc73d9dd9b130d0266d979a3c654cce88dfbff
Alan Xavier <alan_xvr@hotmail.com>
2020-01-27 15:34:03 -0400

    Use React-Router for Navigation 1. Create Login and Register components 2. Updated anchor tags to Links from React-Router 3. Other React-Router setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { Route, Switch, Link } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
+import Login from "./components/auth/Login";
+import Register from "./components/auth/Register";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </div>
   );
 };
