c975110cb15601a0c82ba1de6840636d8c7c8401
Alan Xavier <alan_xvr@hotmail.com>
2020-01-28 17:47:38 -0400

    Create PrivateRoute component 1. Create a PrivateRoute component using react-router 2. Add PrivateRoute to App.js to manage routes requiring authentication

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,8 @@ import Landing from "./components/layout/Landing";
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
+import Dashboard from "./components/dashboard/Dashboard";
+import PrivateRoute from "./components/routing/PrivateRoute";
 
 import { loadUser } from "./redux/auth/auth.actions";
 import setAuthToken from "./utils/auth.utils";
@@ -33,6 +35,7 @@ const App = ({ loadUser }) => {
         <Switch>
           <Route exact path="/register" component={Register} />
           <Route exact path="/login" component={Login} />
+          <PrivateRoute exact path="/dashboard" component={Dashboard} />
         </Switch>
       </section>
     </div>
