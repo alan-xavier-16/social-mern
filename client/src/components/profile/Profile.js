7a875c2c06a652266cd16318845e6bd7e336bca9
Alan Xavier <alan_xvr@hotmail.com>
2020-01-30 19:41:27 -0400

    Add ProfileTop and ProfileAbout components

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -4,6 +4,8 @@ import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import Spinner from "../layout/Spinner";
+import ProfileTop from "./ProfileTop";
+import ProfileAbout from "./ProfileAbout";
 
 import { getProfileById } from "../../redux/profile/profile.actions";
 
@@ -34,6 +36,11 @@ const Profile = ({
                 Edit Profile
               </Link>
             )}
+
+          <div className="profile-grid my-1">
+            <ProfileTop profile={profile} />
+            <ProfileAbout profile={profile} />
+          </div>
         </Fragment>
       )}
     </Fragment>
---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,41 @@
+import React, { Fragment } from "react";
+import PropTypes from "prop-types";
+
+const ProfileAbout = ({
+  profile: {
+    bio,
+    skills,
+    user: { name }
+  }
+}) => {
+  return (
+    <div className="profile-about bg-light p-2">
+      {bio && (
+        <Fragment>
+          <h2 className="text-primary">Bio for {name.trim().split(" ")[0]}</h2>
+          <p>{bio}</p>
+          <div className="line"></div>
+        </Fragment>
+      )}
+
+      {skills && (
+        <Fragment>
+          <h2 className="text-primary">Skill Set</h2>
+          <div className="skills">
+            {skills.map((skill, idx) => (
+              <div key={idx} className="p-1">
+                <i className="fa fa-check"></i> {skill}
+              </div>
+            ))}
+          </div>
+        </Fragment>
+      )}
+    </div>
+  );
+};
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired
+};
+
+export default ProfileAbout;
