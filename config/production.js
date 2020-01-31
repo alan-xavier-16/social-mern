2877865d009065e95696034369902c54c1b08525
Alan Xavier <alan_xvr@hotmail.com>
2020-01-31 17:57:09 -0400

    Set application up for deployment to heroku

---
+++ config/production.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://alanxvr:socialapp@cluster0-eo0bs.mongodb.net/test?retryWrites=true&w=majority",
+  jwtToken: "JWTSecretToken",
+  githubClientId: "ab34dacbea3daec862e1",
+  githubClientSecret: "8b44ce05369e1cc3a8d553229cbc225ac49747c5"
+};
