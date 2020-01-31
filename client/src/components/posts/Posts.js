7bf05abfd3628be642053201102f129ca3b3e4a7
Alan Xavier <alan_xvr@hotmail.com>
2020-01-31 14:36:13 -0400

    Created a PostItem to display from Post

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -1,16 +1,34 @@
 import React, { Fragment, useEffect } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 
 import Spinner from "../layout/Spinner";
+import PostItem from "./PostItem";
 
 import { getPosts } from "../../redux/posts/post.actions";
 
-const Posts = ({ getPosts, post }) => {
+const Posts = ({ getPosts, post: { posts, loading } }) => {
   useEffect(() => {
     getPosts();
   }, [getPosts]);
-  return <div>POSTS</div>;
+  return loading ? (
+    <Spinner />
+  ) : (
+    <>
+      <h1 className="large text-primary">Posts</h1>
+      <p className="lead">
+        <i className="fas fa-user"></i> Welcome to the community!
+      </p>
+
+      {/* PostForm Component */}
+      <div className="posts">
+        {posts.map(post => (
+          <PostItem key={post._id} post={post} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 Posts.propTypes = {
